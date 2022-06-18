7df57888fee0753de8e36c0500e90a05fef5a183
Сергей Харлов <s.harlov@blanc.ru>
2022-06-19 01:59:00 +0500

    bookmark fix

--- src/pages/bookmarks/ui/bookmarks.tsx
+++ src/pages/bookmarks/ui/bookmarks.tsx
@@ -1,22 +1,18 @@
 import React, { FC } from 'react'
 
-import { ITweet } from '@entities/tweet/models/interfaces/Tweet.interface'
-import { useFetch } from '@shared/hooks/useFetch'
-import { Loader } from '@shared/ui/loader/loader'
+import {useSelector} from 'react-redux';
+
+import {likedTweets} from '@features/tweets/tweetsSelectors';
 import { PageHeader } from '@shared/ui/page-header/page-header'
 import { TweetList } from '@widgets/tweet-list/ui/tweet-list'
 
 const Bookmarks: FC = () => {
-  const userData = JSON.parse(localStorage.getItem('userTwitterData') || '')
-  const [isLoading, data] = useFetch<ITweet[]>(
-    `https://62657cf194374a2c5070d523.mockapi.io/api/v1/User/${userData.id}/LikedTweets`,
-    []
-  )
+  const list = useSelector(likedTweets)
 
   return (
     <div>
       <PageHeader pageName={'Bookmarks Page'} />
-      {isLoading ? <Loader /> : <TweetList tweets={data} />}
+      <TweetList tweets={list} />
     </div>
   )
 }
