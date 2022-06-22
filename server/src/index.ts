3386cb8e15df040aacf2dd5e2f950dd3d7cbc1a6
Сергей Харлов <s.harlov@blanc.ru>
2022-06-22 19:03:11 +0500

    endpoint setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
