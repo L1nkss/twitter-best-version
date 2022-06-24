d62d687faeac589d76c7eaca80d4b8f69ffe0b9c
Сергей Харлов <s.harlov@blanc.ru>
2022-06-24 16:50:43 +0500

    chat socket io first

--- client/src/pages/messages/ui/messages.tsx
+++ client/src/pages/messages/ui/messages.tsx
@@ -1,13 +1,76 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
+import io from 'socket.io-client';
+
+import { UserMessageCard } from '@entities/user-message-card/ui/user-message-card';
 import { PageHeader } from '@shared/ui/page-header/page-header'
 
 const Messages: FC = () => {
-  return (
-    <div>
-      <PageHeader pageName={'Messages Page'} />
-    </div>
-  )
+    const [message, setMessage] = useState<string>();
+    const [messages, setMessages] = useState<string[]>([]);
+    const socket = io('http://localhost:8080/');
+
+    const testMockData = [
+        {
+            id: '1',
+            message: 'Привет как дела?',
+            name: 'Имя Фамилия',
+            isOnline: true
+        },
+        {
+            id: '1',
+            message: 'Какой то длинный текст текст текст текст текст текст текст текст',
+            name: 'Имя Фамилия 2 ',
+            isOnline: false
+        }
+    ]
+
+    useEffect(() => {
+        socket.on('client message', (mgs) => {
+            setMessages((state) => [...state, mgs.value])
+        })
+    }, [])
+    
+    const handleInputChanges = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setMessage(evt.target.value);
+    }
+
+    const handleButtonClick = () => {
+        socket.emit('message', {value: message})
+    }
+
+    return (
+        <>
+            <PageHeader pageName={ 'Messages Page' } />
+            {/* Тест*/}
+            <p>Text</p>
+            <input type="text" style={{border: '1px solid black'}} onChange={ handleInputChanges } />
+            <button onClick={ handleButtonClick }>Отправить</button>
+
+            <div className="mb-2">
+                {messages.map((mess, index) => {
+                    return <h2 key={ index }>{mess}</h2>
+                })}
+            </div>
+
+            {/*  Разметка чата  */}
+            <div className="grid grid-cols-12">
+                <div className="message__chats col-span-4">
+                    {testMockData.map((data) => {
+                        return <UserMessageCard
+                            key={ data.id }
+                            message={ data.message }
+                            name={ data.name }
+                            isOnline={ data.isOnline }
+                        />
+                    })}
+                </div>
+                <div className="col-span-8 px-2 ">
+                    <h2>Сообщения</h2>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export { Messages }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,42 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { Socket } from "socket.io";
+import * as http from "http";
 const tweetsRoutes = require('./routes/tweets-route');
 const userRoutes = require('./routes/user-route');
+const app: Express = express();
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
+
+// Socket io
+io.on('connection', (socket: Socket) => {
+    socket.on('message', (mgs) => {
+        io.emit('client message', mgs)
+    })
+});
 
 dotenv.config();
 
-const app: Express = express();
-
 app.use(express.json())
 
-const port = process.env.PORT;
 
 app.get('/api', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
 
+setInterval(()=>{
+    io.to('fromSocket').emit('time', new Date())
+},5000)
+
 // Routes v1
 app.use('/api/v1/tweets', tweetsRoutes);
 app.use('/api/v1/user', userRoutes);
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+server.listen(process.env.PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${process.env.PORT}`);
 });
