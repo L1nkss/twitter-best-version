d735627e305a0a264879c58a30d630ed1df8338b
Сергей Харлов <s.harlov@blanc.ru>
2022-06-25 17:17:55 +0500

    portal for modal

--- client/src/shared/ui/popup/popup.tsx
+++ client/src/shared/ui/popup/popup.tsx
@@ -1,36 +1,37 @@
-import { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import cn from 'classnames'
+
+import { createPortal } from 'react-dom';
 
 import { Icon } from '@shared/ui/icon/icon'
 import { PopupProps } from '@shared/ui/popup/models/interfaces/popup.interface'
 
-export const Popup: FC<PopupProps> = ({
-  onClose,
-  isVisible,
-  title,
-  children,
-}) => {
-  const [show, setShow] = useState(false)
+export const Popup: FC<PopupProps> = ({onClose, isVisible, title, children,}) => {
+    const [ show, setShow ] = useState(false);
 
-  const closeHandler = () => {
-    setShow(false)
-    onClose(false)
-  }
+    const closeHandler = () => {
+        setShow(false)
+        onClose(false)
+    }
 
-  useEffect(() => {
-    setShow(isVisible)
-  }, [isVisible])
+    useEffect(() => {
+        setShow(isVisible)
+    }, [ isVisible ])
 
-  return (
-    <div className={cn('overlay', { 'overlay--show': show })}>
-      <div className="popup">
-        {title && <h2 className="popup__title">{title}</h2>}
-        <span className="popup__close" onClick={closeHandler}>
-          <Icon name="shared/cross-svg" />
-        </span>
-        <div className="popup__content">{children}</div>
-      </div>
-    </div>
-  )
+    if (!show) return null;
+
+    return createPortal(
+        (
+            <div className={ cn('overlay', {'overlay--show': show}) }>
+                <div className="popup">
+                    {title && <h2 className="popup__title">{title}</h2>}
+                    <span className="popup__close" onClick={ closeHandler }>
+                        <Icon name="shared/cross-svg"/>
+                    </span>
+                    <div className="popup__content">{children}</div>
+                </div>
+            </div>
+        ), document.body
+    )
 }
